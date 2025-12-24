a1b90321122ae0a3e1c5c428479827729abacc45
Jason Pamental <jason@rwt.io>
2025-12-24 16:25:14 -0500

    reworking the JS

---
+++ shape-outside-line-box.js
@@ -0,0 +1,31 @@
+function outlineHeader() {
+  const textShape = document.getElementById('text-shape');
+
+  const textShapeRange = document.createRange();
+  textShapeRange.selectNodeContents(textShape);
+
+  const textShapeRects = textShapeRange.getClientRects();
+
+  var shapeString = '';
+
+  for (let i = 0; i < textShapeRects.length; i++) {
+      const textShapeRect = textShapeRects[i];
+    
+          var separator = ``;
+    if (i > 0) {
+      separator = `, `;
+    };
+    shapeString = shapeString + separator + `${textShapeRect.left}px ${textShapeRect.top}px, ${textShapeRect.right}px ${textShapeRect.top}px, ${textShapeRect.right}px ${textShapeRect.bottom}px, ${textShapeRect.left}px ${textShapeRect.bottom}px`;
+
+  }
+  //console.log(shapeString);
+  textShape.style.float = `left`;
+  textShape.style.shapeOutside = `polygon(${shapeString})`;
+  textShape.style.shapeMargin = `.35rem`;
+
+}
+
+window.addEventListener("load", (event) => {
+  outlineHeader();
+});
+
